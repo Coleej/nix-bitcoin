4b366ae9f95625aea0b9845cf0f92ef595b6f10b
Cody Johnson <codyjohnson144@gmail.com>
2026-05-05 22:24:41 -0500

    Enable tailscaled daemon for persistent connectivity

--- configuration.nix
+++ configuration.nix
@@ -89,10 +89,13 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     tailscale
   ];
+
+  # Enable the tailscaled daemon for persistent Tailscale connectivity.
+  services.tailscale.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
