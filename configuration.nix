99d9ed0a846e85e55c84f19fb983aa8fba3bec64
Cody <codyjohnson144@gmail.com>
2026-05-09 14:34:36 -0500

    Re-enable SSH password authentication

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,8 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -72,7 +73,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cody = {
     isNormalUser = true;
-    extraGroups = ["wheel"]; # Enable 'sudo' for the user.
+    extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
     packages = with pkgs; [
       tree
       neovim
@@ -110,7 +111,7 @@
     enable = true;
     settings = {
       PermitRootLogin = "no";
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
     };
   };
 
