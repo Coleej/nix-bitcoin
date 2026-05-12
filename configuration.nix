3b876acf1c4d5061822eab0bb1a2de628fdfc696
Cody Johnson <codyjohnson144@gmail.com>
2026-05-11 22:31:17 -0500

    add 4GB swap for memory pressure

--- configuration.nix
+++ configuration.nix
@@ -154,4 +154,12 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
+
+  # Add swap for memory pressure
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 4096;
+    }
+  ];
 }
